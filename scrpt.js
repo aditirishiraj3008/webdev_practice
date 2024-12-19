2a96a9f9d9c51e23482037df42a866d19e06e5fb
aditirishiraj3008 <aditi1.mitmpl2023@learner.manipal.edu>
2024-12-19 09:37:56 +0000

    dropdown 2 page created

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,4 @@
+//sliding navbar - not working
 const bar = document.querySelector(".fa-bars");
 const menu = document.querySelector(".menu");
 
@@ -5,13 +6,32 @@ bar.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
 });
 
-// Optionally, if you want to toggle dropdowns on click:
 const dropdowns = document.querySelectorAll('.menu > li');
 
 dropdowns.forEach(dropdown => {
     dropdown.addEventListener('click', (e) => {
-        e.stopPropagation(); // Prevent event bubbling
+        e.stopPropagation(); 
         const currentDropdown = dropdown.querySelector('.dropdown');
         currentDropdown.classList.toggle('show-dropdown');
     });
+});
+
+//tabs creation
+const tabs = document.querySelector(".tabs");
+const btns = document.querySelectorAll(".button");
+const articles = document.querySelectorAll(".content");
+tabs.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach(function (btn) {
+      btn.classList.remove("live");
+    });
+    e.target.classList.add("live");
+    // hide others
+    articles.forEach(function (article) {
+      article.classList.remove("live");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("live");
+  }
 });
